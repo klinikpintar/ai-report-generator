91a8d6a8cf7d5c8dcfc902bee175df0a61ccb1c9
Adrian <adrian.hamzah15@gmail.com>
2025-02-26 20:35:43 +0700

    test(schema): add negative test cases for invalid body and duplicate schema

--- __test__/api/schema.test.ts
+++ __test__/api/schema.test.ts
@@ -8,20 +8,45 @@ dotenv.config()
 const BASE_API_URL = process.env.BASE_API_URL || "http://localhost:3000"
 const BASE_SCHEMA_URL = '/api/schema'
 
-const createSchema = async () => {
-  const schemaData = {
-    "name": 'products',
-    "description": 'table to store products data',
-    "schemaText": "CREATE TABLE products (id SERIAL PRIMARY KEY, name TEXT);"
-  }
+const validSchemaData = {
+  name: 'products',
+  description: 'Table to store products data',
+  schemaText: 'CREATE TABLE products (id SERIAL PRIMARY KEY, name TEXT);',
+};
 
+const invalidSchemaData = {
+  name: 'products_invalid',
+};
+
+const createValidSchema = async () => {
   const response = await request(BASE_API_URL)
     .post(BASE_SCHEMA_URL)
-    .send(schemaData)
+    .send(validSchemaData)
 
   expect(response.status).toBe(StatusCodes.CREATED)
 
-  return schemaData.name
+  return validSchemaData.name
+}
+
+const createInvalidSchema = async () => {
+  const response = await request(BASE_API_URL)
+    .post(BASE_SCHEMA_URL)
+    .send(invalidSchemaData)
+
+  expect(response.status).toBe(StatusCodes.BAD_REQUEST)
+
+  return validSchemaData.name
+}
+
+const createDuplicateSchema = async () => {
+  const _ = await request(BASE_API_URL)
+    .post(BASE_SCHEMA_URL)
+    .send(validSchemaData)
+  const duplicateResponse = await request(BASE_API_URL)
+    .post(BASE_SCHEMA_URL)
+    .send(validSchemaData)
+
+  expect(duplicateResponse.status).toBe(StatusCodes.CONFLICT)
 }
 
 const readSchema = async () => {
@@ -43,7 +68,7 @@ const findSchema = async (schemaName: string) => {
 }
 
 const createAndReadSchema = async () => {
-  const schemaName = await createSchema()
+  const schemaName = await createValidSchema()
   await findSchema(schemaName)
 }
 
@@ -52,7 +77,9 @@ beforeEach(async () => {
 });
 
 describe('CR of Schema API', () => {
-  it("should be able to save schema", createSchema)
+  it("should be able to save valid schema", createValidSchema)
+  it("should return BAD REQUEST for invalid schema data", createInvalidSchema)
+  it("should return CONFLICT for duplicate schema", createDuplicateSchema)
   it("should be able to read schema", readSchema)
   it("should be able to integrate save and read (CR) of schema", createAndReadSchema)
 })
