e3035c26677ec641992e1109056f49dbcc8f3246
ilhamelhamdi <ilhamabdillah123@gmail.com>
2025-02-28 21:02:59 +0700

    style: fix pagination style

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -49,9 +49,10 @@ const PaginationLink = ({
     aria-current={isActive ? "page" : undefined}
     className={cn(
       buttonVariants({
-        variant: isActive ? "outline" : "ghost",
+        variant: "ghost",
         size,
       }),
+      isActive ? "bg-[#E4F6FC] text-[#0080FF] hover:bg-[#E4F6FC] hover:text-[#0080FF]" : "text-gray-500",
       className
     )}
     {...props}
@@ -69,8 +70,8 @@ const PaginationPrevious = ({
     className={cn("gap-1 pl-2.5", className)}
     {...props}
   >
-    <ChevronLeft className="h-4 w-4" />
-    <span>Previous</span>
+    <ChevronLeft className="h-8 w-8" />
+    <span>Sebelumnya</span>
   </PaginationLink>
 )
 PaginationPrevious.displayName = "PaginationPrevious"
@@ -85,8 +86,8 @@ const PaginationNext = ({
     className={cn("gap-1 pr-2.5", className)}
     {...props}
   >
-    <span>Next</span>
-    <ChevronRight className="h-4 w-4" />
+    <span>Selanjutnya</span>
+    <ChevronRight className="h-8 w-8" />
   </PaginationLink>
 )
 PaginationNext.displayName = "PaginationNext"
