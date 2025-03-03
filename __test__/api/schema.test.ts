464793e6f03ebbccd737d5078888d36c0e21ce82
Adrian <adrian.hamzah15@gmail.com>
2025-03-03 19:59:02 +0700

    test(schema): add positive test cases for update and delete schema

--- __test__/api/schema.test.ts
+++ __test__/api/schema.test.ts
@@ -14,6 +14,10 @@ const validSchemaData = {
   schemaText: 'CREATE TABLE products (id SERIAL PRIMARY KEY, name TEXT);',
 };
 
+const updatedSchemaData = {
+  description: 'Updated table description',
+};
+
 const invalidSchemaData = {
   name: 'products_invalid',
 };
@@ -57,19 +61,70 @@ const readSchema = async () => {
   expect(Array.isArray(response.body)).toBe(true)
 }
 
-const findSchema = async (schemaName: string) => {
+const findSchema = async (schemaName: string, shouldExist: boolean) => {
   const response = await request(BASE_API_URL)
     .get(BASE_SCHEMA_URL)
 
   expect(response.status).toBe(StatusCodes.OK)
 
   const schemaFound = response.body.find((schema: any) => schema.name === schemaName)
-  expect(schemaFound).toBeDefined()
+
+  if (shouldExist) expect(schemaFound).toBeDefined()
+  else expect(schemaFound).toBeUndefined()
+
+  return schemaFound
 }
 
 const createAndReadSchema = async () => {
   const schemaName = await createValidSchema()
-  await findSchema(schemaName)
+  await findSchema(schemaName, true)
+}
+
+const updateSchema = async (schemaId: number) => {
+  const response = await request(BASE_API_URL).patch(BASE_SCHEMA_URL).send({
+    id: schemaId,
+    ...updatedSchemaData,
+  });
+
+  expect(response.status).toBe(StatusCodes.OK);
+  expect(response.body.description).toBe(updatedSchemaData.description);
+};
+
+const deleteSchema = async (schemaId: number) => {
+  const response = await request(BASE_API_URL).delete(BASE_SCHEMA_URL).send({ id: schemaId });
+
+  expect(response.status).toBe(StatusCodes.OK);
+  expect(response.body.message).toBe('Schema deleted successfully');
+};
+
+const updateSchemaTest = async () => {
+  const _ = await request(BASE_API_URL)
+    .post(BASE_SCHEMA_URL)
+    .send(validSchemaData)
+  
+  await updateSchema(1)
+}
+
+const deleteSchemaTest = async () => {
+  const _ = await request(BASE_API_URL)
+    .post(BASE_SCHEMA_URL)
+    .send(validSchemaData)
+  
+  await deleteSchema(1)
+}
+
+const updateAndFindSchema = async () => {
+  await updateSchemaTest()
+
+  const response = await findSchema("products", true)
+  
+  expect(response.body.description).toBe("Updated table description")
+}
+
+const deleteAndFindSchema = async () => {
+  await deleteSchemaTest()
+
+  await findSchema("products", false)
 }
 
 beforeEach(async () => {
@@ -80,6 +135,13 @@ describe('CR of Schema API', () => {
   it("should be able to save valid schema", createValidSchema)
   it("should return BAD REQUEST for invalid schema data", createInvalidSchema)
   it("should return CONFLICT for duplicate schema", createDuplicateSchema)
+
   it("should be able to read schema", readSchema)
   it("should be able to integrate save and read (CR) of schema", createAndReadSchema)
+
+  it("should be able to update schema", updateSchemaTest)
+  it("should be able to integrate update and read (UR) of schema", updateAndFindSchema)
+
+  it("should be able to delete schema", deleteSchemaTest)
+  it("should be able to integrate delete and read (DR) of schema", deleteAndFindSchema)
 })
