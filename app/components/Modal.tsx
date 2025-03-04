640ccbb24fc02c8972113e04de3724f709ecda96
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2025-03-04 23:18:25 +0700

    feat: add clear form function in modal component

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,9 +1,15 @@
 import React, { ReactNode } from "react";
 import { MouseEvent } from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 interface Props {
   isVisible: boolean;
   onClose: () => void;
+  onClearForm: () => void;
   children: ReactNode;
   title: string;
   subtitle: string;
@@ -12,6 +18,7 @@ interface Props {
 const Modal: React.FC<Props> = ({
   isVisible,
   onClose,
+  onClearForm,
   children,
   title,
   subtitle,
@@ -20,32 +27,37 @@ const Modal: React.FC<Props> = ({
     const target = event.target as HTMLDivElement;
     if (target.id === "wrapper") {
       onClose();
+      onClearForm();
     }
   };
 
-  if (!isVisible) return null;
-
   return (
-    <div
-      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
-      id="wrapper"
-      data-testid="wrapper"
-      onClick={handleClose}
-    >
-      <div className="relative w-full max-w-md max-h-full transition-all">
-        <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+    <main className={inter.className}>
+      <div
+        className={`fixed inset-0 flex justify-center items-center transition-colors ${
+          isVisible ? "visible bg-black/50" : "invisible"
+        }`}
+        id="wrapper"
+        data-testid="wrapper"
+        onClick={handleClose}
+      >
+        <div
+          className={`bg-white w-full max-w-[600px] max-h-full rounded-lg shadow transition-all ${
+            isVisible ? "scale-100 opacity-100" : "scale-105 opacity-0"
+          }`}
+        >
           <div className="flex items-center justify-center p-7 pb-1 rounded-t dark:border-gray-600 border-gray-200 ">
-            <h3 className="text-2xl font-semibold text-[#00B0EB] dark:text-white">
+            <p className="text-[32px] font-bold text-[#00B0EB] dark:text-white">
               {title}
-            </h3>
+            </p>
           </div>
           <div className="flex items-center justify-center md:pl-5 md:pr-5">
-            <p className="text-center">{subtitle}</p>
+            <p className="text-center text-[18px]">{subtitle}</p>
           </div>
           {children}
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
