9327cb600f4689c303197073f12e7b4707dfd8ca
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2025-03-05 22:27:13 +0700

    refactor: update add schema modal to implement SOLID principles

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -6,28 +6,30 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
-interface Props {
+interface BaseModalProps {
   isVisible: boolean;
-  onClose: () => void;
-  onClearForm: () => void;
-  children: ReactNode;
   title: string;
   subtitle: string;
+  children: ReactNode;
+  onClose: () => void;
 }
 
-const Modal: React.FC<Props> = ({
-  isVisible,
-  onClose,
-  onClearForm,
-  children,
-  title,
-  subtitle,
-}) => {
+interface FormModalProps extends BaseModalProps {
+  onClearForm: () => void;
+}
+
+type ModalProps = BaseModalProps | FormModalProps;
+
+const Modal: React.FC<ModalProps> = (props) => {
+  const { isVisible, title, subtitle, children, onClose } = props;
+
   const handleClose = (event: MouseEvent<HTMLDivElement>) => {
     const target = event.target as HTMLDivElement;
     if (target.id === "wrapper") {
       onClose();
-      onClearForm();
+      if ("onClearForm" in props) {
+        props.onClearForm();
+      }
     }
   };
 
@@ -46,10 +48,8 @@ const Modal: React.FC<Props> = ({
             isVisible ? "scale-100 opacity-100" : "scale-105 opacity-0"
           }`}
         >
-          <div className="flex items-center justify-center p-7 pb-1 rounded-t dark:border-gray-600 border-gray-200 ">
-            <p className="text-[32px] font-bold text-[#00B0EB] dark:text-white">
-              {title}
-            </p>
+          <div className="flex items-center justify-center p-7 pb-1 rounded-t border-gray-200 ">
+            <p className="text-[32px] font-bold text-[#00B0EB]">{title}</p>
           </div>
           <div className="flex items-center justify-center md:pl-5 md:pr-5">
             <p className="text-center text-[18px]">{subtitle}</p>
---
+++ app/components/SchemaForm.tsx
@@ -0,0 +1,187 @@
+import React, { FormEvent } from "react";
+
+interface SchemaFormProps {
+  showFileInput?: boolean;
+  fileInputRef: React.RefObject<HTMLInputElement | null>;
+  formData: {
+    name: string;
+    description: string;
+    schemaText: string;
+    fileName?: string;
+  };
+  handleSubmit: (event: FormEvent<HTMLFormElement>) => void;
+  handleChange: (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => void;
+  handleFileChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onClose: () => void;
+  clearForm: () => void;
+}
+
+const SchemaForm: React.FC<SchemaFormProps> = ({
+  showFileInput,
+  fileInputRef,
+  formData,
+  handleSubmit,
+  handleChange,
+  handleFileChange,
+  onClose,
+  clearForm,
+}) => {
+  return (
+    <form className="md:p-10" onSubmit={handleSubmit}>
+      <div className="grid gap-4 mb-4 grid-cols-2">
+        <div className="col-span-2">
+          <label className="block mb-2 text-[14.74px] font-semibold text-gray-900">
+            Nama Skema
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="bg-white border border-gray-300 text-gray-900 text-[16px] rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+            placeholder="Masukkan nama skema"
+            required
+          />
+        </div>
+        <div className="col-span-2">
+          <label className="block mb-2 text-[14.74px] font-semibold text-gray-900">
+            Deskripsi Skema
+          </label>
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            className="block p-2.5 w-full text-[16px] text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Masukkan deskripsi skema"
+            required
+          ></textarea>
+        </div>
+        <div className="col-span-2 sm:col-span-1">
+          <label className="block mb-2 text-[14.74px] font-semibold text-gray-900">
+            Platform Skema
+          </label>
+          <select
+            id="platform"
+            className="bg-[#00B0EB] text-white text-[16.44px] font-bold rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-3 cursor-pointer"
+          >
+            <option value="" className="bg-white" disabled selected>
+              Pilih Platform
+            </option>
+            <option value="PostgreSQL" className="bg-white text-[#00B0EB]">
+              PostgreSQL
+            </option>
+            <option
+              value="MySQL"
+              className="bg-white text-[#00B0EB] hover:bg-gray-50"
+            >
+              MySQL
+            </option>
+            <option value="MongoDB" className="bg-white text-[#00B0EB]">
+              MongoDB
+            </option>
+          </select>
+        </div>
+        <div className="col-span-2 sm:col-span-1">
+          <label className="block mb-2 text-[14.74px] font-semibold text-gray-900">
+            Service Skema
+          </label>
+          <select
+            id="service"
+            className="bg-[#00B0EB] text-white text-[16.44px] font-bold rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-3 cursor-pointer"
+          >
+            <option value="" className="bg-white" disabled selected>
+              Pilih Service
+            </option>
+            <option value="Reservasi" className="bg-white text-[#00B0EB]">
+              Reservasi
+            </option>
+            <option value="Kesehatan" className="bg-white text-[#00B0EB]">
+              Kesehatan
+            </option>
+            <option value="Keuangan" className="bg-white text-[#00B0EB]">
+              Keuangan
+            </option>
+            <option value="Inventaris" className="bg-white text-[#00B0EB]">
+              Inventaris
+            </option>
+          </select>
+        </div>
+        {showFileInput && (
+          <div className="col-span-2">
+            <label className="block mb-2 text-[14.74px] font-semibold text-gray-900">
+              File Skema
+            </label>
+            <label className="flex items-center p-2 justify-center border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-white hover:bg-gray-100">
+              <div className="flex items-center justify-center">
+                <svg
+                  className="pr-1 w-8 h-8 text-gray-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                  />
+                </svg>
+                <p className="text-[16px]">Upload File Skema di sini</p>
+              </div>
+              <input
+                id="dropzone-file"
+                type="file"
+                accept=".sql"
+                onChange={handleFileChange}
+                ref={fileInputRef}
+                className="hidden"
+              />
+            </label>
+            {formData.fileName && (
+              <div className="mt-4 flex items-center justify-between bg-gray-100 p-2 rounded-lg border border-gray-300">
+                <div className="flex items-center space-x-2">
+                  <svg
+                    className="w-8 h-8 text-blue-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 00-.293-.707l-2.5-2.5A1 1 0 0012 2H6zM5 4a1 1 0 011-1h5v2a1 1 0 001 1h2v10a1 1 0 01-1 1H6a1 1 0 01-1-1V4z" />
+                  </svg>
+                  <span className="text-sm font-medium text-gray-700">
+                    {formData.fileName}
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <div className="flex justify-center items-center">
+        <button
+          className="flex-1 text-[#00B0EB] text-[18px] font-bold inline-flex items-center mr-2 ml-5 bg-white hover:text-[#13A1DE] focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full px-5 py-2.5 text-center justify-center border-2 border-[#00B0EB]"
+          onClick={() => {
+            onClose();
+            clearForm();
+          }}
+        >
+          Batal
+        </button>
+        <button
+          type="submit"
+          className="flex-1 text-white text-[18px] font-bold inline-flex items-center mr-5 ml-2 bg-[#00B0EB] hover:bg-[#13A1DE] focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full px-5 py-2.5 text-center justify-center border-2 border-[#00B0EB]"
+        >
+          Simpan
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SchemaForm;
