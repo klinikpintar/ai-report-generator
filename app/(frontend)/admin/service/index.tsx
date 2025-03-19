a29fa43244eb2b06cec7eeb4562926fccf16d1b3
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2025-03-19 23:30:26 +0700

    feat: add service management section

--- app/(frontend)/admin/service/index.tsx
+++ app/(frontend)/admin/service/index.tsx
@@ -3,7 +3,7 @@ import { ServiceSection } from "./sections/service-section";
 
 const ServiceModule = () => {
   return (
-    <section className="container mx-auto flex flex-col gap-y-6 py-8 items-center">
+    <section className="container mx-auto flex flex-col gap-y-6 py-8 items-center mb-5">
       <ServiceSection />
     </section>
   );
--- app/(frontend)/admin/service/sections/service-section.tsx
+++ app/(frontend)/admin/service/sections/service-section.tsx
@@ -2,5 +2,26 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 
 export const ServiceSection = () => {
-  return <></>;
+  return (
+    <>
+      <div className="w-1/2 text-center">
+        <p className="font-semibold text-lg text-[#606060]">
+          Manajemen Service Klinik Pintar
+        </p>
+        <h2 className="text-[#00B0EB] font-bold text-3xl mt-2">
+          Atur Service untuk AI Report Generator
+        </h2>
+        <p className="mt-4">
+          Pastikan chatbot dapat menggunakan service yang sesuai untuk
+          menghasilkan laporan yang akurat dan relevan.
+        </p>
+      </div>
+
+      <div className="flex justify-center">
+        <Button size="lg" className="mr-2 bg-[#00B0EB] hover:bg-[#00B0EB]/90">
+          Kelola Service Sekarang
+        </Button>
+      </div>
+    </>
+  );
 };
