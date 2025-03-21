a30f9a8eec51e361fa1a597cd3f07941dcfb0d17
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2025-03-21 11:27:23 +0700

    feat(footer): add reusable Footer component with researcher & developers section

--- app/(frontend)/admin/layout.tsx
+++ app/(frontend)/admin/layout.tsx
@@ -1,9 +1,11 @@
+import Footer from "@frontend/components/footer";
 import React, { Suspense } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen bg-white flex flex-col">
+    <div className="min-h-screen flex flex-col bg-white overflow-y-auto">
       <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+      <Footer />
     </div>
   );
 };
