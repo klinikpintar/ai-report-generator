e0ee744e8b137e26bf0adaeeb60ee931a1561fe2
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2025-04-07 23:06:44 +0700

    refactor: menghilangkan code smell pada sonarqube

--- app/(backend)/dtos/users.dto.ts
+++ app/(backend)/dtos/users.dto.ts
@@ -18,13 +18,13 @@ export class UserValidation {
     page: z
       .string()
       .optional()
-      .transform((val) => parseInt(val || "1"))
+      .transform((val) => parseInt(val ?? "1"))
       .refine((val) => val > 0, { message: "Page must be greater than 0" }),
 
     limit: z
       .string()
       .optional()
-      .transform((val) => parseInt(val || "10"))
+      .transform((val) => parseInt(val ?? "10"))
       .refine((val) => val > 0, { message: "Limit must be greater than 0" }),
 
     role: z
