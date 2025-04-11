254eafda236705fbf9c46a3c216d0da46eec24b4
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-04-11 15:10:16 +0700

    feat: integrate filter user by role with user table

--- app/(frontend)/admin/manage-user/components/FilterByRoleDropdown.tsx
+++ app/(frontend)/admin/manage-user/components/FilterByRoleDropdown.tsx
@@ -2,8 +2,23 @@
 import { FilterDropdown } from "@frontend/components/FilterDropdown";
 import React from "react";
 import { UserRole } from "../types/user";
+import { useUserTableContext } from "../context/UserTableContext";
 
 export const FilterByRoleDropdown = () => {
+  const { dispatch, state } = useUserTableContext();
+
+  const handleRoleChange = (selectedRoles: UserRole[]) => {
+    dispatch({
+      type: "SET_FILTERS",
+      payload: {
+        role: {
+          ...state.filters.role,
+          selected: selectedRoles,
+        },
+      },
+    });
+  };
+
   const displayRole = (role: UserRole) => {
     switch (role) {
       case "BUSINESS_ANALYST":
@@ -15,8 +30,8 @@ export const FilterByRoleDropdown = () => {
   return (
     <FilterDropdown<UserRole>
       items={Object.values(UserRole)}
-      selectedItems={[]}
-      onSelectionChange={() => {}}
+      selectedItems={state.filters.role.selected}
+      onSelectionChange={handleRoleChange}
       getKey={(role) => role}
       buttonText="Filter by Role"
       renderItem={(role) => displayRole(role)}
