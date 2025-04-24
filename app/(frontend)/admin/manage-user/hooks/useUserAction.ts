9011e08794b1d87a6303c5f59713a85ebc609ee4
Adrian <adrian.hamzah15@gmail.com>
2025-04-24 17:52:03 +0700

    fix(edit-acc-modal): fix eslint error

--- app/(frontend)/admin/manage-user/hooks/useUserAction.ts
+++ app/(frontend)/admin/manage-user/hooks/useUserAction.ts
@@ -1,6 +1,9 @@
+import { User } from "../types/user";
+
 export const useUserActions = () => {
-  const handleDeleteUser = (user: any) => {
+  const handleDeleteUser = (user: User) => {
     // WIP
+    console.log(user.name)
     return true
   }
   return {
--- app/(frontend)/admin/manage-user/page.tsx
+++ app/(frontend)/admin/manage-user/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 import React from "react";
 import { UserManagementSection } from "@frontend/admin/manage-user/sections/userManagementSection";
-import { AddAccountButton } from "@frontend/admin/manage-user/components/addAccountButton";
-import { UserTable } from "./components/UserTable";
-import { FilterByRoleDropdown } from "./components/FilterByRoleDropdown";
 import { UserTableProvider } from "./context/UserTableContext";
 import { UserTableSection } from "./sections/userTableSection";
 
