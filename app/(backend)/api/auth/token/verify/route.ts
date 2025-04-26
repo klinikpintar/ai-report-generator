1bfeae75c534c2ded60a9379901c33795d339c66
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2025-04-26 11:21:01 +0700

    refactor: memastikan name, status dan email di return

--- app/(backend)/api/auth/token/verify/route.ts
+++ app/(backend)/api/auth/token/verify/route.ts
@@ -8,14 +8,10 @@ export async function GET(req: Request) {
   try {
     const token = extractToken(cookie, "access_token");
     const user = await authService.verify(token);
-
-    return NextResponse.json(
-      {
-        data: { user },
-        message: "Token verified",
-      },
-      { status: 200 }
-    );
+    return NextResponse.json({
+      message: "Token is valid",
+      data: user,
+    }, { status: 200 });
   } catch (error) {
     if (error instanceof ErrorResponse) {
       return error.generate();
