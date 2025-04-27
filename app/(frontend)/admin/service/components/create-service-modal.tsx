e9a11ad7584730d00795468934c5dca66cedabfc
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-04-28 00:17:04 +0700

    feat: refresh filter by service dropdown in schema table after adding new service

--- app/(frontend)/admin/service/components/create-service-modal.tsx
+++ app/(frontend)/admin/service/components/create-service-modal.tsx
@@ -7,6 +7,7 @@ import { ConfirmationDialog } from "@frontend/admin/schema/components/confirmati
 import FormInput from "@frontend/components/form-input";
 import SelectInput from "@frontend/components/select-input";
 import ButtonSubmit from "@frontend/components/button-submit";
+import { eventBus, EVENTS } from "@frontend/common/utils/event-bus";
 
 interface Props {
   isVisible: boolean;
@@ -125,6 +126,7 @@ const CreateServiceModal = ({ isVisible, onClose }: Props) => {
         toast.success("Service successfully added");
         setFormData({ name: "", platform: "" });
         onClose();
+        eventBus.publish(EVENTS.SERVICE_UPDATED); // Notify other components
       } else {
         toast.error(`Failed to submit service: ${response.statusText}`);
       }
