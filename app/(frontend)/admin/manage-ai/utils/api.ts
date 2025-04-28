132968011b00f6c9e7c99bc4d0ef5060b094b36b
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-04-29 01:46:23 +0700

    [GREEN] feat(ai-model): implemented patchApiKey function

--- app/(frontend)/admin/manage-ai/utils/api.ts
+++ app/(frontend)/admin/manage-ai/utils/api.ts
@@ -10,5 +10,28 @@ type PatchApiKeyResponse = {
 }
 
 export const patchApiKey = async ({ providerId, apiKey }: PatchApiKeyParams) => {
-  
+  try {
+    const response = await fetch('/api/manage-ai/api-key', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id_ai: providerId,
+        api_key: apiKey,
+      }),
+    });
+    const data: PatchApiKeyResponse = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+
+    return data;
+  } catch (error) {
+    return {
+      success: false,
+      message: (error as Error).message,
+    };
+  }
 }
