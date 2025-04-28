6cac8d8118568b7232e1c3350c0858399b323c02
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-04-29 01:39:54 +0700

    [RED] test(ai-model): add test for edit api key form modal

---
+++ app/(frontend)/admin/manage-ai/components/EditAPIKeyModal.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  isVisible: boolean;
+  onClose: () => void;
+  providerId: string;
+  providerName: string;
+}
+
+export const EditAPIKeyModal = ({ isVisible, onClose, providerId, providerName }: Props) => {
+  return <></>;
+};
