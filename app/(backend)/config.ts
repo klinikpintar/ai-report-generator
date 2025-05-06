ddc4e35fe5c566247ce66d91755d22b4d30846a0
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-05-06 11:57:21 +0700

    fix(build): remove enforcement to set ENCRYPTION_KEY on production for passing CI

--- app/(backend)/config.ts
+++ app/(backend)/config.ts
@@ -8,10 +8,7 @@ const config = {
   JWT_REFRESH_EXPIRES: timeConvertMs(process.env.JWT_REFRESH_EXPIRES ?? "7d"),
   NODE_ENV: process.env.NODE_ENV ?? "development",
   BCRYPT_SALT_ROUNDS: parseInt(process.env.BCRYPT_SALT_ROUNDS ?? '10', 10),
-  ENCRYPTION_KEY: process.env.ENCRYPTION_KEY ??
-    (process.env.NODE_ENV === "production" ?
-      (() => { throw new Error("ENCRYPTION_KEY must be set in production") })() :
-      crypto.randomBytes(32).toString('hex')),
+  ENCRYPTION_KEY: process.env.ENCRYPTION_KEY ?? crypto.randomBytes(32).toString('hex'),
 };
 
 export default config;
